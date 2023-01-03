d07bf1014e384dbf17537a35bc4db4898b519c50
viettran1010 <viettran10101988@gmail.com>
2023-01-03 09:39:47 +0700

    fix: redirect

--- client/src/page/CreateBattle.jsx
+++ client/src/page/CreateBattle.jsx
@@ -13,7 +13,9 @@ const CreateBattle = () => {
   const [waitBattle, setWaitBattle] = useState(false);
 
   useEffect(() => {
-    if (gameData?.activeBattle?.battleStatus === 0) {
+    if (gameData?.activeBattle?.battleStatus === 1) {
+      navigate(`/battle/${gameData.activeBattle.name}`);
+    } else if (gameData?.activeBattle?.battleStatus === 0) {
       setWaitBattle(true);
     }
   }, [gameData]);
